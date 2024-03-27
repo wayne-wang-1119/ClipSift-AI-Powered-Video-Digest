295571242f3f32fc51414486b0e17c4ce19f6b7b
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2024-03-26 19:02:58 -0700

    update snippet_selector.py

--- transcript_based_auto_clip/snippet_selector.py
+++ transcript_based_auto_clip/snippet_selector.py
@@ -1,12 +1,18 @@
 from dotenv import load_dotenv
 import os
 import openai
+import weaviate
+import json
 
 load_dotenv()
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+client = weaviate.Client(
+    url="http://localhost:8080", additional_headers={"X-OpenAI-Api-Key": openai.api_key}
+)
+
 
 def embed_transcripts(video_urls):
-    
+    pass
